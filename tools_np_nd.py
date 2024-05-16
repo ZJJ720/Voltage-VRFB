4ca94d907bcc97300b65eeb5f608bb54f734c774
Jiajie Zhu <873307929@qq.com>
2024-05-16 11:53:13 +0800

    标况训练下的BiLSTM对比模型

--- tools_np_nd.py
+++ tools_np_nd.py
@@ -5,29 +5,53 @@ import pandas as pd
 import numpy as np
 
 
-def create_dataset(excel_index):
+def create_train_dataset():
     # 读取 excel 数据
-    df = pd.read_excel(f'data/test{excel_index}_nd.xlsx') # 假设你的数据文件名为 data.xlsx
+    df = pd.read_excel(f'data/train_dc.xlsx')
     # 前 5 列数据读取为 dataX
-    dataX = df.iloc[:, :4].values
+    dataX = df.iloc[:, [0,1,2,4,5]].values
     # 第 6 列数据读取为 dataY
-    dataY = df.iloc[:, 4].values
+    dataY = df.iloc[:, 6].values
     # 将 dataX, dataY 变量转变为 tensor 类型
     dataX = torch.from_numpy(dataX).float()
     dataY = torch.from_numpy(dataY).float().reshape(-1, 1)
+
 
     dataset = np.concatenate((dataX, dataY), axis=1)
 
     return dataset
 
 
-def scale(train, test):
-    scaler = MinMaxScaler(feature_range=(-1, 1))
+def create_test_dataset():
+    # 读取 excel 数据
+    df = pd.read_excel(f'data/test_dc.xlsx')
+    # 前 5 列数据读取为 dataX
+    dataX = df.iloc[:, [0,1,2,4,5]].values
+    # 第 6 列数据读取为 dataY
+    dataY = df.iloc[:, 6].values
+    # 将 dataX, dataY 变量转变为 tensor 类型
+    dataX = torch.from_numpy(dataX).float()
+    dataY = torch.from_numpy(dataY).float().reshape(-1, 1)
+
+
+    dataset = np.concatenate((dataX, dataY), axis=1)
+
+    return dataset
+
+
+def train_scale(train):
+    scaler = MinMaxScaler(feature_range=(0, 1))
     scaler = scaler.fit(train)
-    train_scaled = scaler.transform(train)
+    train_scaled = scaler.fit_transform(train)
+    return scaler, train_scaled
+
+
+def test_scale(test):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler = scaler.fit(test)
     test = test.reshape(test.shape[0], test.shape[1])
-    test_scaled = scaler.transform(test)
-    return scaler, train_scaled, test_scaled
+    test_scaled = scaler.fit_transform(test)
+    return scaler, test_scaled
 
 
 def invert_scale(scaler: object, X: object, value: object) -> object:
@@ -44,7 +68,7 @@ class DataPrepare(Dataset):
     def __init__(self, train):
         self.len = train.shape[0]
         x_set = train[:, 0:-1]
-        x_set = x_set.reshape(x_set.shape[0], 1, 4)
+        x_set = x_set.reshape(x_set.shape[0], 1, 5)
         # 数据类型转为 torch 变量
         self.x_data = torch.from_numpy(x_set)
         self.y_data = torch.from_numpy(train[:, [-1]])
